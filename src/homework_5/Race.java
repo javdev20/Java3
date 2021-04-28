ca607af344ae7edd93a6a65ccbd80314d707cf02
Islam <islomiddin.kayumov@bk.ru>
2021-04-28 13:11:16 +0500

    homework 5

--- src/homework_5/Race.java
+++ src/homework_5/Race.java
@@ -7,6 +7,8 @@ public class Race {
 
     private ArrayList<Stage> stages;
     public ArrayList<Stage> getStages() { return stages; }
+
+
     public Race(Stage... stages) {
         this.stages = new ArrayList<>(Arrays.asList(stages));
     }
