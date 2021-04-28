074fbaf4dac6891ef78e05e1a303fc793d704c61
javdev20 <71741007+javdev20@users.noreply.github.com>
2021-04-28 13:37:26 +0500

    Update Car.java

--- src/homework_5/Car.java
+++ src/homework_5/Car.java
@@ -8,18 +8,35 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Car implements Runnable{
 
     private static int CARS_COUNT;
+    private static boolean winnerFound;
+    private static Lock win = new ReentrantLock();
+    
     static {
         CARS_COUNT = 0;
     }
+    
     private Race race;
     private int speed;
     private String name;
-    public String getName() {
-        return name;
+    private CyclicBarrier cb;
+    private CountDownLatch cdl;
+    
+    Car(Race race, int speed, CyclicBarrier cb, CountDownLatch cdl) {
+        this.race = race;
+        this.speed = speed;
+        CARS_COUNT++;
+        this.name = "Участник #" + CARS_COUNT;
+        this.cb = cb;
+        this.cdl = cdl;
     }
-    public int getSpeed() {
-        return speed;
+    
+    private static synchronized void checkWinner(Car c) {
+        if (!winnerFound) {
+            System.out.println(c.name + " - WIN");
+            winnerFound = true;
+        }
     }
+   
     public Car(Race race, int speed) {
         this.race = race;
         this.speed = speed;
@@ -30,14 +47,27 @@ public class Car implements Runnable{
     public void run() {
         try {
             System.out.println(this.name + " готовится");
-            Thread.sleep(500 + (int)(Math.random() * 800));
+            Thread.sleep(500 + (int) (Math.random() * 800));
             System.out.println(this.name + " готов");
+            cb.await();
+            cb.await();
+            for (int i = 0; i < race.getStages().size(); i++) {
+                race.getStages().get(i).go(this);
+            }
+            checkWinner(this);
+            cb.await();
+    
         } catch (Exception e) {
             e.printStackTrace();
         }
-        for (int i = 0; i < race.getStages().size(); i++) {
-            race.getStages().get(i).go(this);
-        }
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public int getSpeed() {
+        return speed;
     }
 }
 
